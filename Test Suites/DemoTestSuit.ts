88d47ecff7adf8d453dbb80c5efc3a883b9efb1b
Suchand <suchand.pv@mgtechsoft.com>
2021-12-07 22:01:24 +0530

    demo commit

---
+++ Test Suites/DemoTestSuit.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>for learning</description>
+   <name>DemoTestSuit</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c22df6aa-01d7-4410-8f07-7552539a8e57</testSuiteGuid>
+   <testCaseLink>
+      <guid>98ecbebd-a4f7-4bcf-aa58-ce9306df19b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/firstDemoTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>01b95f9a-9b06-4cb2-9544-4f7e66080e37</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/firstDemoTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
